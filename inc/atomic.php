cfd63b490d617a15fbca0a8fc274851e80598671
Tammie Lister <karmatosed@gmail.com>
2015-01-30 00:48:53 +0000

    Link into atomic.php

--- inc/atomic.php
+++ inc/atomic.php
@@ -5,14 +5,18 @@
 * @package atomic_s
 */
 // For now lets go ghetto and just include each atom
-require get_template_directory() . '/elements/molecules/navigation/nav-post.php';
 
+require get_template_directory() . '/elements/molecules/forms/search-form.php';
+require get_template_directory() . '/elements/molecules/navigation/nav-post.php';
 require get_template_directory() . '/elements/molecules/text/entry-footer.php';
 require get_template_directory() . '/elements/molecules/text/posted-on.php';
 
+require get_template_directory() . '/elements/organisms/comments/comments-list.php';
+require get_template_directory() . '/elements/organisms/comments/comments-title.php';
 require get_template_directory() . '/elements/organisms/comments/nav-comment-below.php';
 require get_template_directory() . '/elements/organisms/comments/nav-comment-above.php';
 require get_template_directory() . '/elements/organisms/global/branding.php';
 require get_template_directory() . '/elements/organisms/global/site-info.php';
 require get_template_directory() . '/elements/organisms/sections/archive-description.php';
 require get_template_directory() . '/elements/organisms/sections/archive-title.php';
+require get_template_directory() . '/elements/organisms/sections/not-found.php';
