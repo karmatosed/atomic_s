d7ccd07f0ea56bb7b98f7c48a2e3da2037557c8b
Tammie Lister <karmatosed@gmail.com>
2015-01-30 00:42:34 +0000

    Readme

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @package atomic_s
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php atomic_s_notfound(); ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
