72455df1eed2d7617a908025a7b27e3efb951901
Tammie Lister <karmatosed@gmail.com>
2015-01-30 00:00:20 +0000

    Adds in text atoms
    - Entry footer
    - Archive description
    - Archive title

--- inc/atomic.php
+++ inc/atomic.php
@@ -6,4 +6,9 @@
 */
 // For now lets go ghetto and just include each atom
 require get_template_directory() . '/elements/molecules/navigation/nav-post.php';
+
+require get_template_directory() . '/elements/molecules/text/entry-footer.php';
 require get_template_directory() . '/elements/molecules/text/posted-on.php';
+
+require get_template_directory() . '/elements/organisms/sections/archive-description.php';
+require get_template_directory() . '/elements/organisms/sections/archive-title.php';
