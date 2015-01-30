b8fbd3762edc7f9f174f8a71c5ab354fb0d3463c
Tammie Lister <karmatosed@gmail.com>
2015-01-30 00:49:47 +0000

    Link in atomic.php

--- functions.php
+++ functions.php
@@ -136,3 +136,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+* Get Atomic.
+*/
+require get_template_directory() . '/inc/atomic.php';
