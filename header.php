443219d9c97c52ca8083f4066443131e05feb4e2
Tammie Lister <karmatosed@gmail.com>
2015-01-30 00:16:35 +0000

    Header atoms

--- header.php
+++ header.php
@@ -22,15 +22,11 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'atomic_s' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'atomic_s' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+		<?php atomic_s_branding(); ?>
+
+		<?php atomic_s_primarynav(); ?>
+		
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
