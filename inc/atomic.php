95561eafdc2f9a0918c3002778f917ba1a934f00
Tammie Lister <karmatosed@gmail.com>
2015-03-03 14:18:16 +0000

    Revision to structure
    Brings into play new bricks, buildings and structures construction.

--- inc/atomic.php
+++ inc/atomic.php
@@ -6,17 +6,17 @@
 */
 // For now lets go ghetto and just include each atom
 
-require get_template_directory() . '/elements/molecules/forms/search-form.php';
-require get_template_directory() . '/elements/molecules/navigation/nav-post.php';
-require get_template_directory() . '/elements/molecules/text/entry-footer.php';
-require get_template_directory() . '/elements/molecules/text/posted-on.php';
+require get_template_directory() . '/elements/buildings/forms/search-form.php';
+require get_template_directory() . '/elements/buildings/navigation/nav-post.php';
+require get_template_directory() . '/elements/buildings/text/entry-footer.php';
+require get_template_directory() . '/elements/buildings/text/posted-on.php';
 
-require get_template_directory() . '/elements/organisms/comments/comments-list.php';
-require get_template_directory() . '/elements/organisms/comments/comments-title.php';
-require get_template_directory() . '/elements/organisms/comments/nav-comment-below.php';
-require get_template_directory() . '/elements/organisms/comments/nav-comment-above.php';
-require get_template_directory() . '/elements/organisms/global/branding.php';
-require get_template_directory() . '/elements/organisms/global/site-info.php';
-require get_template_directory() . '/elements/organisms/sections/archive-description.php';
-require get_template_directory() . '/elements/organisms/sections/archive-title.php';
-require get_template_directory() . '/elements/organisms/sections/not-found.php';
+require get_template_directory() . '/elements/structures/comments/comments-list.php';
+require get_template_directory() . '/elements/structures/comments/comments-title.php';
+require get_template_directory() . '/elements/structures/comments/nav-comment-below.php';
+require get_template_directory() . '/elements/structures/comments/nav-comment-above.php';
+require get_template_directory() . '/elements/structures/global/branding.php';
+require get_template_directory() . '/elements/structures/global/site-info.php';
+require get_template_directory() . '/elements/structures/sections/archive-description.php';
+require get_template_directory() . '/elements/structures/sections/archive-title.php';
+require get_template_directory() . '/elements/structures/sections/not-found.php';
